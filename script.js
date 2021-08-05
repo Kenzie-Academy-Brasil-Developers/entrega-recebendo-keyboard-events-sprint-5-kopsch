b67268519d91706b83f4c216df2cf6f75bdda18b
pedrokopsch <pedrokopsch@gmail.com>
2021-08-05 18:35:19 -0300

    recursos adicionados

--- script.js
+++ script.js
@@ -8,35 +8,39 @@ console.log(container)
 const moveGIF = (pressedKey) => {
     if(pressedKey === 'ArrowUp'){
       boxTop-= 10
-      document.getElementById("box").style.top = boxTop + "px";
+      document.querySelector("#box").style.top = boxTop + "px";
     }
     if(pressedKey === 'ArrowDown'){
       boxTop+= 10
-      document.getElementById("box").style.top = boxTop + "px";
+      document.querySelector("#box").style.top = boxTop + "px";
     }
     if(pressedKey === 'ArrowLeft'){
       boxLeft-= 10
-      document.getElementById("box").style.left = boxLeft + "px";
+      document.querySelector("#box").style.left = boxLeft + "px";
+      document.querySelector("#box").style.transform = "scaleX(-1)"
     }
     if(pressedKey === 'ArrowRight'){
       boxLeft+= 10
-      document.getElementById("box").style.left = boxLeft + "px"; 
+      document.querySelector("#box").style.left = boxLeft + "px"; 
+      document.querySelector("#box").style.transform = "none"
     }
     if(pressedKey === 'w'){
         boxTop-= 10
-        document.getElementById("box").style.top = boxTop + "px";
+        document.querySelector("#box").style.top = boxTop + "px";
     }
     if(pressedKey === 's'){
         boxTop+= 10
-        document.getElementById("box").style.top = boxTop + "px";
+        document.querySelector("#box").style.top = boxTop + "px";
     }
     if(pressedKey === 'a'){
         boxLeft-= 10
-        document.getElementById("box").style.left = boxLeft + "px";
+        document.querySelector("#box").style.left = boxLeft + "px";
+        document.querySelector("#box").style.transform = "scaleX(-1)"
     }
     if(pressedKey === 'd'){
         boxLeft+= 10
-        document.getElementById("box").style.left = boxLeft + "px"; 
+        document.querySelector("#box").style.left = boxLeft + "px"; 
+        document.querySelector("#box").style.transform = "none"
    }
   }
   
